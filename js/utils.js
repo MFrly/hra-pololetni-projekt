21a3ec0cb167990f979f845e195027cae0df63ea
Femmy :3 <118221254+MFrly@users.noreply.github.com>
2024-01-22 18:33:20 +0100

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,7 @@
+// Převedení jednorozměrného pole na dvourozměrné pole
 Array.prototype.parse2D= function(){
     const rows = []
+    // Rozdělení pole na řádky s délkou 16 prvků
     for(let i = 0; i < this.length; i+=16){
         rows.push(this.slice(i, i + 16))
     }
@@ -7,12 +9,14 @@ Array.prototype.parse2D= function(){
     return rows
 }
 
-
+// Vytvoření objektů kolize z dvourozměrného pole
 Array.prototype.createObjectsFrom2D = function(){
     const objects = []
     this.forEach((row, y )=> {
         row.forEach((symbol, x) => {
+             // Kontrola, zda symbol odpovídá kolizi
             if(symbol === 292 || symbol === 250){
+                // Vytvoření nového objektu kolize a přidání do pole objektů
                 // protlačení nové kolize do bloků kolize
                 objects.push(
                     new CollisionBlock({
