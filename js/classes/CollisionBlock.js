4ccbe82afb6bb603f1d42993b5875cf6ac86c94d
Femmy :3 <118221254+MFrly@users.noreply.github.com>
2024-01-22 18:35:41 +0100

    Update CollisionBlock.js

--- js/classes/CollisionBlock.js
+++ js/classes/CollisionBlock.js
@@ -1,9 +1,12 @@
+// Definování třídy pro kolizní blok
 class CollisionBlock{
+    // Konstruktor s pozicí jako parametrem
     constructor({position}){
         this.position = position
         this.width = 64
         this.height = 64
     }
+        // Vykreslení kolizního bloku
     draw(){
         c.fillStyle='rgba(255,0,0,0.5)'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
